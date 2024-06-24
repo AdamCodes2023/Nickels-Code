690e827ff69c14338b188594bd987c52cb6b4928
AdamCodes2023 <122576141+AdamCodes2023@users.noreply.github.com>
2024-06-24 15:35:36 -0400

    Create nickels.js

---
+++ nickels.js
@@ -0,0 +1,19 @@
+/*
+First time? Check out the tutorial game:
+https://sprig.hackclub.com/gallery/getting_started
+*/
+
+/* 
+Nickels by Adam Miller. Made on June 24th, 2024.
+Nickels is a fun die game that has players compete to be the first one to fill a board.
+Players roll two dice and cover up the matching sum on their board with a nickel.
+If a player already has already covered the number that he or she rolls,
+the next player has a chance to steal that value for his or her own board.
+Play continues to the next player after the stealer.
+If a player rolls the same number on both dice and does not get stolen from,
+he or she gets to roll again.
+If a player rolls a 7, a random nickel is removed from his or her own board.
+The first player to fill all ten spaces on the board is the winner!
+In this digital version, all decisions are handled randomly and immediately.
+The goal is to beat the computer opponent.
+*/
